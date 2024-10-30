72ac230d34c1dbcecb653ce3d8f734612da02ea6
Chatchapong Thuaypha <chatchapong1@windowslive.com>
2024-10-30 02:27:15 -0700

    Initial commit

---
+++ Activity26/function.cpp
@@ -0,0 +1,8 @@
+//
+//  function.cpp
+//  Activity26
+//
+//  Created by Chatchapong Thuaypha on 10/28/24.
+//
+
+#include <stdio.h>
---
+++ Activity26/header.h
@@ -0,0 +1,12 @@
+//
+//  header.h
+//  Activity26
+//
+//  Created by Chatchapong Thuaypha on 10/28/24.
+//
+
+#ifndef header_h
+#define header_h
+
+
+#endif /* header_h */
